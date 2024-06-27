11dcfd549e10d689bd5a5efaf6296146590525a9
@yohanesrinaldy209 <izenostudy93@gmail.com>
2024-06-27 23:02:54 +0700

    add redines dan livenes

--- express-app/app.js
+++ express-app/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// Endpoint untuk readiness check
+app.get('/readiness', (req, res) => {
+  // Tambahkan logika untuk memeriksa apakah aplikasi siap
+  res.status(200).send('Ready');
+});
+
+// Endpoint untuk liveness check
+app.get('/liveness', (req, res) => {
+  // Tambahkan logika untuk memeriksa apakah aplikasi hidup
+  res.status(200).send('Alive');
+});
+
 app.get('/profile', (req, res) => {
   const nama = process.env.Nama;
   if (nama) {
